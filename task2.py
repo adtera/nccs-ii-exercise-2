task2 gradient fix

task2.py
@@ -9,7 +9,6 @@ import jax.numpy as npj
 
 
 print('=== DEFINING FUNCTIONS')
-count = 0
 def E_potential(position):
     print('--- Start function: E_potential')
     position = np.reshape(position,(M,3))    
@@ -23,35 +22,19 @@ def E_potential(position):
     Alpha = 3.028
     r_e = 1.411
     print('calculating V_Morse')
-    ##
-#    E_pot = 0
-#    for _r in r:
-#        E_pot += D_e * (npj.exp(-2*Alpha*(_r-r_e))-2*npj.exp(-Alpha*(_r-r_e)))
-    ##
     V_Morse = D_e * (npj.exp(-2*Alpha*(r-r_e))-2*npj.exp(-Alpha*(r-r_e)))
     E_pot = sum(V_Morse)
-    count += 1
+    count[0] += 1
     print(f'iter: {count}')
     print('--- Exit function: E_potential')
     return E_pot
 
-def energy_gradient2(position):
-    print(position)
-    return jax.jit(jax.grad(E_potential))
-
-
-def energy_gradient(position):
-    print('--- Start function: energy_gradient')
-    morse_gradient = jax.jit(jax.grad(E_potential))
-    print('--- Exit function: energy_gradient')
-    return morse_gradient(position)
-    
 ## Input arguments
 def parse_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument("length", help="name of input txt file")
-    parser.add_argument("number", help="length of a single timestep")
-    parser.add_argument("temperature", help="numer of time steps")
+    parser.add_argument("length", help="length of box")
+    parser.add_argument("number", help="number of particles")
+    parser.add_argument("temperature", help="temperature in kelvin")
     return parser.parse_args()
 
 np.random.seed(800)
@@ -64,10 +47,11 @@ if args:
     M = int(args.number)
     T = float(args.temperature)
 else:
-    L = 2 #int(input())
-    M = 100 #int(input())
+    L = 15 #int(input())
+    M = 1000 #int(input())
     T = 300 #int(input())
 
+count = [0]
 
 config = (L,M)
 print(f"values: L = {L}, M = {M}, T = {T}")
@@ -79,25 +63,29 @@ position = particles.get_array().flatten()
 #coordinates = npj.array(coords)#
 
 options = {
-    'gtol': 1e-2,
+    'gtol': 1.19e-7,
     'disp': True,
-#    'maxiter': 3,
+#    'maxiter': 30,
+#    'norm': inf,
     'return_all': True
     }
 
 print('=== START SCIPY.OPTIMIZE.MINIMIZE')
+
+energy_gradient = jax.jit(jax.grad(E_potential))
+
 res = minimize(
     E_potential,
     position,
 #    args = config,
     method = "CG",
-    jac = energy_gradient,
+    jac = energy_gradient, #jax.jit()
     options = options
     )
 print('=== EXIT SCIPY.OPTIMIZE.MINIMIZE')
 
 
-pos_input = np.array(res.x).reshape(M[0], 3)
+pos_input = np.array(res.x).reshape(M, 3)
 
 ##### velocity
 k = 3.166811429 * (10 ** -6)
@@ -106,7 +94,7 @@ mu = np.array([0.0, 0.0, 0.0])
 sigma = np.array([varr, varr, varr])
 covariance = np.diag(sigma**2)
 
-vels = np.random.multivariate_normal(mean= mu, cov=covariance, size=(M[0],1))
+vels = np.random.multivariate_normal(mean= mu, cov=covariance, size=(M,1))
 #vels = vels.reshape(np.array(coords).shape)
 vels_0 = vels - vels.mean(axis=0, keepdims=True)
 
@@ -127,8 +115,8 @@ print(vel_input)
 #%%
 print(res)
 #%%
-print(E_potential(coords))
-print(E_potential(vel_input))
+print(E_potential(position))
+print(E_potential(pos_input))
 #print(np.array(new_coords))
 
 #%%