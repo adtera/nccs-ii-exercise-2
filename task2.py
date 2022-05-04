07c555007675c0fd1eec3619cd2f86cd82677b9c
atezer <armin.tezer@gmail.com>
2022-05-04 12:39:14 +0200

    task2 refactor

--- task2.py
+++ task2.py
@@ -2,20 +2,19 @@
 import numpy as np
 from scipy.optimize import minimize
 import matplotlib.pyplot as plt
-import seaborn as sns
-from utils import particle_cloud
-# E_potential, acceleration
-import scipy
+from utils import particle_cloud, save_to_file
+import argparse
 import jax
 import jax.numpy as npj
-import argparse
+
+  
 
 def E_potential(position):
-#    position = position.reshape((M, 3))
-    position = np.reshape(position,(M[0],3))
+    position = np.reshape(position,(M,3))
     delta = position[:, npj.newaxis, :] - position
     indices = npj.triu_indices(position.shape[0], k=1)
     delta = delta[indices[0], indices[1], :]
+    delta = delta - L * npj.round(delta/L)
     r2 = (delta * delta).sum(axis=1)
     r = npj.sqrt(r2)
     D_e = 1.6
@@ -25,43 +24,62 @@ def E_potential(position):
     E_pot = sum(V_Morse)
     return E_pot
 
-# Acceleration
-def acceleration(position):
+def energy_gradient(position):
     morse_gradient = jax.jit(jax.grad(E_potential))
     forces = morse_gradient(position)
-    #accel = forces/m
-    #return accel
     return np.array(forces)
 
-def out_string(position,velocity):
-    output = np.empty([len(position), 6])
-    trjct = ""
-    for i in range(len(position)):
-        for j in range(0,3) :
-            output[i,j] = position[i,j]
-            output[i,j+3] = velocity[i,j]
-    for line in output:
-        for elem in line:
-            trjct += str(elem) + " "
-        trjct += "\n"
-    return trjct
+## Input arguments
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("length", help="name of input txt file")
+    parser.add_argument("number", help="length of a single timestep")
+    parser.add_argument("temperature", help="numer of time steps")
+    return parser.parse_args()
 
-def save_to_file(position,velocity):
-
-    with open("input.txt","w") as file:
-        file.write(str(M[0]) + '\n')
-        file.write("-" + '\n')
-        file.write(str(L) + '\n')
-        file.write(out_string(position,velocity))       
-        
 np.random.seed(800)
-L = 2 #int(input())
-M = (15,) #int(input())
-T = 300 #int(input())
-particles = particle_cloud(M[0], L, T)
+try:
+    args = parse_arguments()
+except:
+    args = False
+
+if args:
+    L = float(args.length)
+    M = int(args.number)
+    T = float(args.temperature)
+else:
+    L = 15 #int(input())
+    M = 1000 #int(input())
+    T = 300 #int(input())
+
+config = (L,M)
+particles = particle_cloud(L, M, T)
 coords = particles.get_array()
 
 #k = scipy.constants.physical_constants["Boltzmann constant in eV/K"][0]
+#coordinates = npj.array(coords)#
+coordinates = np.array(coords).flatten()
+
+options = {
+    'gtol': 1e-7,
+    'disp': True,
+    'maxiter': 3,
+    'return_all': True
+    }
+print(config)
+
+res = minimize(
+    E_potential,
+    coordinates,
+#    args = config,
+    method = "CG",
+    jac = energy_gradient,
+    options = options
+    )
+
+pos_input = np.array(res.x).reshape(M[0], 3)
+
+##### velocity
 k = 3.166811429 * (10 ** -6)
 varr = np.sqrt((k*T)/18.998403)
 
@@ -79,30 +97,24 @@ velssss = vels - vels.mean(axis=0, keepdims=True)
 
 velssss = np.array(velssss)
 
-#coordinates = npj.array(coords)#
-coordinates = np.array(coords).flatten()
 
-options = {
-    'gtol': 1e-7,
-    'disp': True,
-    'return_all': True
-    }
-res = minimize(
-    E_potential,
-    coordinates,
-    method = "CG",
-    jac = acceleration,
-#    jac = acceleration,
-    options = options
-    )
+vel_input = np.array([vel for [vel] in velssss])
 
-new_coords = np.array(res.x).reshape(M[0], 3)
 
-vel_output = np.array([vel for [vel] in velssss])
+save_to_file(pos_input, vel_input)
 
-save_to_file(new_coords, vel_output)
+
+
 #%%
 print("DONE")
+
+
+
+
+#%%
+
+print(new_coords)
+print(vel_output)
 #%%
 print(res)
 #%%
